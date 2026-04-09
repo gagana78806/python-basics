c411a82998b413725b132b76ea357f0fc7b843a7
gagana78806 <gaga78806@gmail.com>
2026-04-09 20:50:42 +0530

    Added sum program
    This repository contains simple Python programs created while learning basics.

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-basics
-My beginner Python programs
---
+++ sum_two-numbers.py
@@ -0,0 +1,7 @@
+# sum of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum is:", sum)
